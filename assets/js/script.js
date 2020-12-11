bcdcbcfa5e68142be23016604e549b9840c50c98
bamithprdxn <amithkumarb@prdxn.com>
2020-12-11 18:35:56 +0530

    Js file Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,23 @@
 */
 
 
+$(document).ready(function () {
+    
+    $('.menu-links').removeClass("active");
+    console.log()
+    $(".menu-links").click(function () {
+        //    console.log(this.innerHTML);
+        let value = $(this).attr("data-filter");
+        console.log(value);
 
+        let list = $(".menu-list-li");
+        console.log(list.hasClass(value))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (list.hasClass(value)) {
+            $(list).not("." + value).hide();
+            $(list).filter("." + value).show();
+        }
+        
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+})
